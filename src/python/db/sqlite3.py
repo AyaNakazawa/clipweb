7807b62271068c84c9512111b7025d084faad113
ayayaya <aya@ayatec.jp>
2017-12-31 00:42:43 +0900

    Add base python scripts

---
+++ src/python/db/sqlite3.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- mode: python; Encoding: utf-8; coding: utf-8 -*-
+
+"""
+----------------------------------------------------------------
+SQLite3 utility
+  Author: Aya Nakazawa
+  GitHub: https://github.com/AyaNakazawa
+----------------------------------------------------------------
+"""
+
+# ----------------------------------------------------------------
+# Import
+# ----------------------------------------------------------------
+
+import sqlite3
+import json
+
+# ----------------------------------------------------------------
+# Class
+# ----------------------------------------------------------------
+
+class SQLite3:
+
+    DB_PATH = 'clipweb.db'
+
+    def __init__:
+        conn = sqlite3.connect(cls.DB_PATH)
+        c = conn.cursor()
+
+        insert = "insert into users(hash, name) values(?,?)"
+        user = ("aaaa", "bbbb")
+        c.execute(insert, user)
+
+        conn.commit()
+
+        select = 'select * from users'
+        for row in c.execute(select):
+            print(row)
+
+        conn.close()
---
+++ src/python/test.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- mode: python; Encoding: utf-8; coding: utf-8 -*-
+
+print("Content-type: text/html; charset=UTF-8")
+print()
+
+print("test")
