dc5fe3d70816a2ce54f8bf45e74bc918236138ba
ayayaya <aya@ayatec.jp>
2018-01-02 21:09:33 +0900

    Update python

--- src/python/clipweb.py
+++ src/python/clipweb.py
@@ -36,7 +36,7 @@ class Clipweb:
         print("[INIT]")
         print("clipweb")
         _mode = cgi.CGI.get("mode")
-        _mode = "user.register"
+        _mode = "USER.REGISTER"
 
         if _mode is not None:
             cls.check_mode(_mode)
@@ -44,15 +44,15 @@ class Clipweb:
     def check_mode(cls, _mode=None):
         # print(cls._mode)
         _mode = _mode.split(".")
-        if _mode[0] == "user":
+        if _mode[0] == "USER":
             # print("user")
             cls.check_user(_mode[1])
 
-        elif _mode[0] == "clip":
+        elif _mode[0] == "CLIP":
             # print("clip")
             cls.check_clip(_mode[1])
 
-        elif _mode[0] == "share":
+        elif _mode[0] == "SHARE":
             # print("share")
             cls.check_share(_mode[1])
 
@@ -63,23 +63,23 @@ class Clipweb:
     def check_user(cls, _mode=None):
         USER = user.User()
         # print("check user")
-        if _mode == "register":
+        if _mode == "REGISTER":
             # print("register")
             USER.register()
 
-        elif _mode == "login":
+        elif _mode == "LOGIN":
             # print("login")
             USER.login()
 
-        elif _mode == "setting":
+        elif _mode == "SETTING":
             # print("setting")
             USER.setting()
 
-        elif _mode == "info":
+        elif _mode == "INFO":
             # print("info")
             USER.info()
 
-        elif _mode == "leave":
+        elif _mode == "LEAVE":
             # print("leave")
             USER.leave()
 
@@ -90,15 +90,15 @@ class Clipweb:
     def check_clip(cls, _mode=None):
         CLIP = clip.Clip()
         # print("check clip")
-        if _mode == "new":
+        if _mode == "NEW":
             # print("new")
             CLIP.new()
 
-        elif _mode == "save":
+        elif _mode == "SAVE":
             # print("save")
             CLIP.save()
 
-        elif _mode == "delete":
+        elif _mode == "DELETE":
             # print("delete")
             CLIP.delete()
 
@@ -109,19 +109,19 @@ class Clipweb:
     def check_share(cls, _mode=None):
         SHARE = share.Share()
         # print("check share")
-        if _mode == "alive":
+        if _mode == "ALIVE":
             # print("alive")
             SHARE.alive()
 
-        elif _mode == "read":
+        elif _mode == "READ":
             # print("read")
             SHARE.read()
 
-        elif _mode == "write":
+        elif _mode == "WRITE":
             # print("write")
             SHARE.write()
 
-        elif _mode == "leave":
+        elif _mode == "LEAVE":
             # print("leave")
             SHARE.leave()
 
