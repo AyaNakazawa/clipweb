97aa2231b88c4301de6928ef0e107fe0c516fb13
ayayaya <aya@ayatec.jp>
2018-01-04 18:41:22 +0900

    Add mylib Time system
    Add TIME
     - INIT
     - LOAD
     - READY
     - START

--- src/js/mylib/myapp.js
+++ src/js/mylib/myapp.js
@@ -1,4 +1,32 @@
 
+TIME.LOAD = new Date();
+
 $(() => {
+  TIME.READY = new Date();
+
+  Log.log();
+  Log.logClass(`${Project.NAME} (${Project.KEY})`, 'Init');
+
   PS = new Process();
+
+  TIME.START = new Date();
+
+  Log.log();
+  Log.logClassKey(
+    'Load',
+    new Date(TIME.LOAD - TIME.INIT).formatString('%S.%MSs'),
+    new Date(TIME.LOAD - TIME.INIT).formatString('%S.%MSs')
+  );
+  Log.logClassKey(
+    'Ready',
+    new Date(TIME.READY - TIME.LOAD).formatString('%S.%MSs'),
+    new Date(TIME.READY - TIME.INIT).formatString('%S.%MSs')
+  );
+  Log.logClassKey(
+    'Start',
+    new Date(TIME.START - TIME.READY).formatString('%S.%MSs'),
+    new Date(TIME.START - TIME.INIT).formatString('%S.%MSs')
+  );
+  Log.log();
+  Log.logClass(`${Project.NAME} (${Project.KEY})`, 'Start');
 });
