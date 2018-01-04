3e848234f42028437a0f1978122ce734b36e2ecd
ayayaya <aya@ayatec.jp>
2018-01-04 18:02:04 +0900

    Update extension
    Update Date

--- src/js/mylib/extension.js
+++ src/js/mylib/extension.js
@@ -1,12 +1,14 @@
 
-Date.prototype.toString = function (_format = '%Y/%m/%d %H:%M:%S') {
+Date.prototype.formatString = function (_format = '%Y/%m/%d(%W) %H:%M:%S.%MS') {
   _format = _format.replace('%Y', ('000' + this.getFullYear()).slice(-4));
   _format = _format.replace('%y', ('0' + this.getFullYear()).slice(-2));
   _format = _format.replace('%m', ('0' + (this.getMonth() + 1)).slice(-2));
   _format = _format.replace('%d', ('0' + this.getDate()).slice(-2));
+  _format = _format.replace('%W', (this.toLocaleString(window.navigator.language, {weekday: 'short'})));
   _format = _format.replace('%H', ('0' + this.getHours()).slice(-2));
   _format = _format.replace('%M', ('0' + this.getMinutes()).slice(-2));
   _format = _format.replace('%S', ('0' + this.getSeconds()).slice(-2));
+  _format = _format.replace('%MS', ('000' + this.getMilliseconds()).slice(-4));
   return _format;
 };
 
