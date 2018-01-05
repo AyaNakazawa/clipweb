4b67ba144c7b19961b3178f32b1a8b5c9b99f813
ayayaya <aya@ayatec.jp>
2018-01-05 16:28:16 +0900

    Upgrade Extension
    Upgrade
    Add
     - Object.prototype.getType
     - Object.prototype.typeIs

--- src/js/mylib/extension.js
+++ src/js/mylib/extension.js
@@ -1,5 +1,18 @@
 
-Date.prototype.formatString = function (_format = '%Y/%m/%d(%W) %H:%M:%S.%MS') {
+// ----------------------------------------------------------------
+// Extension
+
+let EX = {};
+EX.Date = {};
+EX.String = {};
+EX.Object = {};
+
+// ----------------------------------------------------------------
+// Define
+
+EX.Date.formatString = function (
+  _format = '%Y/%m/%d(%W) %H:%M:%S.%MS'
+) {
   _format = _format.replace('%Y', ('000' + this.getFullYear()).slice(-4));
   _format = _format.replace('%y', ('0' + this.getFullYear()).slice(-2));
   _format = _format.replace('%m', ('0' + (this.getMonth() + 1)).slice(-2));
@@ -12,10 +25,49 @@ Date.prototype.formatString = function (_format = '%Y/%m/%d(%W) %H:%M:%S.%MS') {
   return _format;
 };
 
-String.prototype.capitalize = function () {
+EX.String.capitalize = function () {
   return this.substring(0, 1).toUpperCase() + this.substring(1).toLowerCase();
 };
 
-String.prototype.getRows = function () {
+EX.String.getRows = function () {
   return this.length - this.replace(/\n/g, '').length + 1;
 };
+
+/**
+ * Object.prototype.getType
+ * @param  {Object} obj  Inspect Object
+ * @return {String}      Result
+**/
+
+EX.Object.getType = function(obj) {
+  return Object.prototype.toString.call(obj).slice(8, -1);
+};
+
+/**
+ * Object.prototype.typeIs
+ * @param  {String}  type Object type
+ * @param  {Object}  obj  Object to check
+ * @return {Boolean}      Check result
+**/
+
+EX.Object.typeIs = function(type, obj) {
+  const _CLAS = Object.prototype.toString.call(obj).slice(8, -1);
+  return obj !== undefined && obj !== null && _CLAS === type;
+};
+
+// ----------------------------------------------------------------
+// defineProperty
+
+(() => {
+  for (let type of Object.keys(EX)) {
+    for (let extension of Object.keys(EX[type])) {
+      Object.defineProperty(
+        eval(`${type}.prototype`),
+        extension,
+        {
+          value: EX[type][extension]
+        }
+      );
+    }
+  }
+})();
