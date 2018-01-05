562091356661138f238897b5815b67fb4dc5b5a9
ayayaya <aya@ayatec.jp>
2018-01-05 20:52:54 +0900

    Fix typo
    Fix docstring to Controllers

--- src/js/mylib/controllers.js
+++ src/js/mylib/controllers.js
@@ -3,24 +3,24 @@
 /**
  * Confirm
  * Doc: https://getbootstrap.com/docs/4.0/components/modal/#options
- * @param  {String}  NAME          Name of Object
- * @param  {String}  id            id of confirm selector
- * @param  {String}  title         Title of modal
- * @param  {String}  content       Content of modal
- * @param  {String}  type          Type of button number (0, 1, 2)
- * @param  {String}  show          Show modal when init
- * @param  {String}  backdrop      Includes backdrop (true, false, 'static')
- * @param  {String}  keyboard      Close modal when escape key pressed
- * @param  {String}  focus         Focus modal when init
- * @param  {String}  selector      Selector of Event
- * @param  {String}  trigger       Trigger of Event
- * @param  {String}  image         Image data of modal
- * @param  {String}  yes           Name of Yes button
- * @param  {String}  no            Name of No button
- * @param  {String}  functionYes   Function run when Yes pressed
- * @param  {String}  functionNo    Function run when No pressed
- * @param  {String}  functionClose Function run when closed
- * @return {Confirm}               Extends ConfirmController, CommonController, Commonclass
+ * @param  {String}         NAME          Name of Object
+ * @param  {String}         id            id of confirm selector
+ * @param  {String}         title         Title of modal
+ * @param  {String}         content       Content of modal
+ * @param  {Number}         type          Type of button number (0, 1, 2)
+ * @param  {Boolean}        show          Show modal when init
+ * @param  {Boolean|String} backdrop      Includes backdrop (true, false, 'static')
+ * @param  {Boolean}        keyboard      Close modal when escape key pressed
+ * @param  {Boolean}        focus         Focus modal when init
+ * @param  {String}         selector      Selector of Event
+ * @param  {String}         trigger       Trigger of Event
+ * @param  {String}         image         Image data of modal
+ * @param  {String}         yes           Name of Yes button
+ * @param  {String}         no            Name of No button
+ * @param  {Function}       functionYes   Function run when Yes pressed
+ * @param  {Function}       functionNo    Function run when No pressed
+ * @param  {Function}       functionClose Function run when closed
+ * @return {Confirm}                      Extends ConfirmController, CommonController, Commonclass
 **/
 
 class Confirm extends ConfirmController {
@@ -99,14 +99,14 @@ class Popover extends PopoverController {
 /**
  * Scroll
  * Doc: https://getbootstrap.com/docs/4.0/components/popovers/#options
- * @param  {String}  NAME           NAME of Object
- * @param  {Number}  selector       Selector of Scroll
- * @param  {String}  speed          Speed of Scroll
- * @param  {String}  easing         Easing of Scroll
- * @param  {String}  offset         Offset of Scroll
- * @param  {String}  eventSelector  Selector of Event
- * @param  {String}  eventTrigger   Trigger of Event
- * @return {Scroll}                 Extends ScrollController, CommonController, Commonclass
+ * @param  {String} NAME           NAME of Object
+ * @param  {Number} selector       Selector of Scroll
+ * @param  {Number} speed          Speed of Scroll
+ * @param  {String} easing         Easing of Scroll
+ * @param  {Number} offset         Offset of Scroll
+ * @param  {String} eventSelector  Selector of Event
+ * @param  {String} eventTrigger   Trigger of Event
+ * @return {Scroll}                Extends ScrollController, CommonController, Commonclass
 **/
 
 class Scroll extends ScrollController {
@@ -130,9 +130,9 @@ class Scroll extends ScrollController {
  * Switch
  * Doc: https://getbootstrap.com/docs/4.0/components/popovers/#options
  * @param  {String}  NAME            NAME of Object
- * @param  {Number}  selector        selector of Switch
- * @param  {String}  view            view when init
- * @param  {String}  speed           speed of Switch
+ * @param  {String}  selector        selector of Switch
+ * @param  {Boolean} view            view when init
+ * @param  {Number}  speed           speed of Switch
  * @param  {String}  template        template of Switch
  * @param  {String}  localStorageKey localStorageKey of Switch view
  * @param  {String}  eventTrigger    Trigger of Event
