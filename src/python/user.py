0ca88f3889271f0640dba21367e014d83e2310c9
ayaya <aya@ayatec.jp>
2018-01-14 01:23:45 +0900

    Update py User, Add define the clipweb user error

--- src/python/user.py
+++ src/python/user.py
@@ -26,6 +26,7 @@ class User:
     # Define
 
     def __init__(cls):
+        cls._define()
         cls.result = {}
         cls.DB_PATH = "db/clipweb.db"
         cls.DB = flex_sqlite3.FlexSQLite3(cls.DB_PATH)
@@ -65,3 +66,172 @@ class User:
         cls.result["type"] = "leave"
         cls.result["result"] = True
         return cls.result
+
+    # ----------------------------------------------------------------
+    # Inner Function
+    # ----------------------------------------------------------------
+
+    def _define(cls):
+        cls.NAME = "clipweb user"
+        cls.ERROR = {
+            "code_not_defined": {
+                "{} error".format(cls.NAME): {
+                    "code": 51,
+                    "message": "Error code does not defined."
+                }
+            },
+            "hash_not_defined": {
+                "{} error".format(cls.NAME): {
+                    "code": 101,
+                    "message": "[hash] does not defined."
+                }
+            },
+            "username_not_defined": {
+                "{} error".format(cls.NAME): {
+                    "code": 102,
+                    "message": "[username] does not defined."
+                }
+            },
+            "email_address_not_defined": {
+                "{} error".format(cls.NAME): {
+                    "code": 103,
+                    "message": "[email_address] does not defined."
+                }
+            },
+            "password_hash_not_defined": {
+                "{} error".format(cls.NAME): {
+                    "code": 104,
+                    "message": "[password_hash] does not defined."
+                }
+            },
+            "encrypted_crypto_hash_not_defined": {
+                "{} error".format(cls.NAME): {
+                    "code": 105,
+                    "message": "[encrypted_crypto_hash] does not defined."
+                }
+            },
+            "theme_not_defined": {
+                "{} error".format(cls.NAME): {
+                    "code": 106,
+                    "message": "[theme] does not defined."
+                }
+            },
+            "default_owner_publish_not_defined": {
+                "{} error".format(cls.NAME): {
+                    "code": 107,
+                    "message": "[default_owner_publish] does not defined."
+                }
+            },
+            "default_clip_mode_not_defined": {
+                "{} error".format(cls.NAME): {
+                    "code": 108,
+                    "message": "[default_clip_mode] does not defined."
+                }
+            },
+            "hash_unknown_class": {
+                "{} error".format(cls.NAME): {
+                    "code": 201,
+                    "message": "[hash] is unknown class"
+                }
+            },
+            "username_unknown_class": {
+                "{} error".format(cls.NAME): {
+                    "code": 202,
+                    "message": "[username] is unknown class"
+                }
+            },
+            "email_address_unknown_class": {
+                "{} error".format(cls.NAME): {
+                    "code": 203,
+                    "message": "[email_address] is unknown class"
+                }
+            },
+            "password_hash_unknown_class": {
+                "{} error".format(cls.NAME): {
+                    "code": 204,
+                    "message": "[password_hash] is unknown class"
+                }
+            },
+            "encrypted_crypto_hash_unknown_class": {
+                "{} error".format(cls.NAME): {
+                    "code": 205,
+                    "message": "[encrypted_crypto_hash] is unknown class"
+                }
+            },
+            "theme_unknown_class": {
+                "{} error".format(cls.NAME): {
+                    "code": 206,
+                    "message": "[theme] is unknown class"
+                }
+            },
+            "default_owner_publish_unknown_class": {
+                "{} error".format(cls.NAME): {
+                    "code": 207,
+                    "message": "[default_owner_publish] is unknown class"
+                }
+            },
+            "default_clip_mode_unknown_class": {
+                "{} error".format(cls.NAME): {
+                    "code": 208,
+                    "message": "[default_clip_mode] is unknown class"
+                }
+            },
+            "username_overlap": {
+                "{} error".format(cls.NAME): {
+                    "code": 301,
+                    "message": "Username already exists."
+                }
+            },
+            "email_address_overlap": {
+                "{} error".format(cls.NAME): {
+                    "code": 302,
+                    "message": "User Email address already exists."
+                }
+            },
+            "hash_overlap": {
+                "{} error".format(cls.NAME): {
+                    "code": 303,
+                    "message": "User hash already exists."
+                }
+            }
+        }
+
+    def _error(
+        cls,
+        code=None,
+        *arguments,
+        **keyword_arguments
+    ):
+        if code is None:
+            return cls._error("code_not_defined", mode=sys._getframe().f_code.co_name)
+
+        error = cls.ERROR[code]
+
+        for i_args in range(len(arguments)):
+            custom_message = "Custom Message {0}".format(i_args)
+            error["{} error".format(cls.NAME)][custom_message] = arguments[i_args]
+
+        for key in keyword_arguments.keys():
+            error["{} error".format(cls.NAME)][key] = keyword_arguments[key]
+
+        return error
+
+    def _check_str(
+        cls,
+        model=None,
+        type=str,
+        not_defined_error=None,
+        unknown_class_error=None
+    ):
+        if isinstance(model, type):
+            if len(model) == 0:
+                cls.result["error"] = cls._error(not_defined_error)
+                return False
+        elif model is None:
+            cls.result["error"] = cls._error(not_defined_error)
+            return False
+        else:
+            cls.result["error"] = cls._error(unknown_class_error)
+            return False
+
+        return True
