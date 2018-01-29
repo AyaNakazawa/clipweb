333b7bcc00b186204b52cd11b3c4c7b39852a4f2
ayayaya <aya@ayatec.jp>
2018-01-29 09:42:03 +0900

    Add : js Code

---
+++ src/js/myapp/code.js
@@ -0,0 +1,234 @@
+
+// ----------------------------------------------------------------
+// Code Class
+
+// ----------------------------------------------------------------
+// Model
+
+class CodeModel extends CodewebModel {
+  constructor (
+    initSetting = {
+      NAME: 'Code Object'
+    }
+  ) {
+    super(initSetting);
+
+    // ----------------------------------------------------------------
+    // エラーコード
+
+    // ----------------------------------------------------------------
+    // 識別子
+    this.KEY = 'code';
+
+    // ----------------------------------------------------------------
+    // LocalStorageキー
+
+    // ----------------------------------------------------------------
+    // 情報
+
+    // クリップ
+    this.HASH = '';
+    this.FILENAME = '';
+    this.FILETYPE = '';
+    this.DATA = '';
+
+    this.DL_DATA = null;
+
+    // ----------------------------------------------------------------
+    // テンプレート
+    this.TEMPLATE.EDITOR = '#code-editor-template';
+
+    // ----------------------------------------------------------------
+    // セレクタ
+
+    // エリア
+    this.SELECTOR.AREA = '#code-editor-area';
+
+    // 新規作成
+    this.SELECTOR.EDITOR = {};
+    this.SELECTOR.EDITOR.SHARE = '#code-editor-share';
+    this.SELECTOR.EDITOR.SETTING = '#code-editor-setting';
+
+  }
+}
+
+// ----------------------------------------------------------------
+// View
+
+class CodeView extends CodewebView {
+  constructor (
+    initSetting = {
+      NAME: 'Code View'
+    }
+  ) {
+    super(initSetting);
+  }
+}
+
+// ----------------------------------------------------------------
+// Event
+
+class CodeEvent extends CodewebEvent {
+  constructor (
+    initSetting = {
+      NAME: 'Code Event'
+    }
+  ) {
+    super(initSetting);
+  }
+
+  setEvent () {
+    this.setOnHide();
+  }
+
+  // ----------------------------------------------------------------
+  // hide
+
+  setOnHide () {
+    super.setHeaderButton({
+      func: () => {
+        this.trigger({ trigger: this.MODEL.TRIGGER.VIEW.HIDE });
+      }
+    });
+  }
+
+  // ----------------------------------------------------------------
+  // type
+
+  setOnEditor () {
+
+  }
+}
+
+// ----------------------------------------------------------------
+// Controller
+
+class CodeController extends CodewebController {
+  constructor (
+    model = {},
+    initSetting = {
+      NAME: 'Code Controller',
+      MODEL: new CodeModel(),
+      VIEW: new CodeView(),
+      EVENT: new CodeEvent()
+    }
+  ) {
+    super(model, initSetting);
+    this.init();
+  }
+
+  init () {
+    this.EVENT.setEvent();
+  }
+
+  // ----------------------------------------------------------------
+  // ajax
+
+  // ----------------------------------------------------------------
+  // code
+
+  decrypto () {
+    this.MODEL.DATA = this.MODEL.DL_DATA;
+  }
+
+  share () {
+    super.log(this.MODEL.HASH.substr(0, 14), 'Share', Log.ARROW_INPUT)();
+  }
+
+  setting () {
+    super.log(this.MODEL.HASH.substr(0, 14), 'Setting', Log.ARROW_INPUT)();
+    CLIP.connectSetting(this.MODEL.TYPE.LOAD, this.MODEL.HASH);
+  }
+
+  // ----------------------------------------------------------------
+  // model
+
+  getAreaModel (
+    type = null
+  ) {
+    if (type == null) {
+      Log.error(arguments)();
+      return;
+    }
+    let _result = {};
+    switch (type) {
+      case this.MODEL.TYPE.OPEN:
+        // OPEN
+        _result = {
+          hash: this.MODEL.HASH,
+          filename: this.MODEL.FILENAME,
+          filetype: this.MODEL.FILETYPE,
+          filetypes: this.MODEL.FILETYPES,
+        };
+        break;
+
+      default:
+        Log.error(arguments, 'unknown type X(')();
+        return;
+    }
+    return _result;
+  }
+
+  applyReceiveModel (
+    type = null
+  ) {
+    super.log(type.capitalize(), 'Apply')();
+    if (type == null) {
+      Log.error(arguments);
+      return;
+    }
+    if (type != this.getAjaxData({ key: 'type' })) {
+      Log.error(arguments, 'type mismatch X(')();
+      return;
+    }
+
+    switch (type) {
+      case this.MODEL.TYPE.OPEN:
+        // OPEN
+        this.MODEL.DL_DATA = this.getAjaxData({ key: 'data' });
+        this.decrypto();
+        this.MODEL.FILENAME = this.getAjaxData({ key: 'filename' });
+        this.MODEL.FILETYPE = this.getAjaxData({ key: 'filetype' });
+        break;
+
+      case this.MODEL.TYPE.SAVE:
+        // SAVE
+        this.MODEL.DL_DATA = this.getAjaxData({ key: 'data' });
+        this.decrypto();
+        break;
+
+      default:
+        Log.error(arguments, 'unknown type X(')();
+        return;
+    }
+  }
+
+  getSendModel (
+    type = null
+  ) {
+    if (type == null) {
+      Log.error(arguments)();
+      return;
+    }
+    let _model = {};
+    _model['owner_hash'] = USER.MODEL.HASH.USER;
+    _model['password_hash'] = USER.MODEL.HASH.PASSWORD;
+    switch (type) {
+      case this.MODEL.TYPE.OPEN:
+        // OPEN
+        _model['hash'] = this.MODEL.HASH;
+        break;
+
+      case this.MODEL.TYPE.SAVE:
+        // SAVE
+        _model['hash'] = this.MODEL.HASH;
+        _model['data'] = this.MODEL.DATA;
+        break;
+
+      default:
+        Log.error(arguments, 'unknown type X(')();
+        return;
+    }
+    return _model;
+  }
+}
