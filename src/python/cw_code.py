3a50f225ce71c3feaf039f1eb73db97acf3d5348
ayaya <aya@ayatec.jp>
2018-02-03 15:36:44 +0900

    Update : js Code : method save : It works!

--- src/python/cw_code.py
+++ src/python/cw_code.py
@@ -139,5 +139,102 @@ class Code(cw_base.Base):
     def save(cls):
         cls.result["type"] = sys._getframe().f_code.co_name
         cls.result["result"] = False
+
+        # ----------------------------------------------------------------
+        # cgi get
+
+        user_hash = cls.cgi.get("owner_hash")
+        user_password_hash = cls.cgi.get("password_hash")
+        clip_hash = cls.cgi.get("hash")
+        clip_data = cls.cgi.get("data")
+
+        if clip_data is None:
+            clip_data = ""
+
+        # ----------------------------------------------------------------
+        # cgi get strings check
+
+        if cls._check_str(
+            model=user_hash,
+            not_defined_error="owner_hash_not_defined",
+            unknown_class_error="owner_hash_unknown_class"
+        ) is False:
+            return cls.result
+
+        if cls._check_str(
+            model=user_password_hash,
+            not_defined_error="password_hash_not_defined",
+            unknown_class_error="password_hash_unknown_class"
+        ) is False:
+            return cls.result
+
+        if cls._check_str(
+            model=clip_hash,
+            not_defined_error="hash_not_defined",
+            unknown_class_error="hash_unknown_class"
+        ) is False:
+            return cls.result
+
+        if cls._check_str(
+            model=clip_data,
+            unknown_class_error="data_unknown_class"
+        ) is False:
+            return cls.result
+
+        # ----------------------------------------------------------------
+        # count user
+
+        num_user_data = cls.DB.count_records(
+            table="owners",
+            where={
+                "hash": user_hash,
+                "password_hash": user_password_hash
+            }
+        )
+
+        if num_user_data > 1:
+            cls.result["error"] = cls._error("corrupt_userdata")
+            return cls.result
+
+        if num_user_data < 1:
+            cls.result["error"] = cls._error("user_not_found")
+            return cls.result
+
+        # ----------------------------------------------------------------
+        # auth
+
+        auth = cls.DB.select(
+            table="clips",
+            column=[
+                "owner_hash",
+                "clip_mode"
+            ],
+            where={
+                "hash": clip_hash
+            }
+        )
+
+        auth = auth[0]
+        if auth["owner_hash"] != user_hash:
+            if auth["clip_mode"] != 'share':
+                cls.result["error"] = cls._error("permission_denied")
+                return cls.result
+
+        # ----------------------------------------------------------------
+        # save data
+
+        cls.result["save"] = cls.DB.update(
+            table="clips",
+            value={
+                "data": clip_data
+            },
+            where={
+                "hash": clip_hash
+            }
+        )
+
+        # ----------------------------------------------------------------
+        # return
+
         cls.result["result"] = True
         return cls.result
