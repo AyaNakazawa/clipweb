92bdceb935e55031545a1f27a923469a34a8256f
ayaya <aya@ayatec.jp>
2018-02-04 19:10:53 +0900

    Add : js User : function init : Add message for share

--- src/js/myapp/user.js
+++ src/js/myapp/user.js
@@ -416,6 +416,9 @@ class UserController extends ClipwebController {
         }
       }
     }
+    if (Web.getParam('share') != null) {
+      this.VIEW.toast({ type: 'info', position: 'center', message: LN.get('need_login_to_share') });
+    }
     this.open({ type: this.MODEL.TYPE.LOGIN });
   }
 
