ecde88ed784475cf636f0a646ac9927f8856f242
ayaya <aya@ayatec.jp>
2018-02-12 17:28:53 +0900

    Add : py Code : method sync

--- src/python/cw_code.py
+++ src/python/cw_code.py
@@ -14,6 +14,7 @@ clipweb Code
 # ----------------------------------------------------------------
 
 import sys
+import datetime
 from db import flex_sqlite3
 import cw_base
 
@@ -247,6 +248,144 @@ class Code(cw_base.Base):
 
         cls.result["result"] = True
         return cls.result
+
+    def sync(cls):
+        cls.result["type"] = sys._getframe().f_code.co_name
+        cls.result["result"] = False
+
+        # ----------------------------------------------------------------
+        # get & check cgi
+
+        user_hash = cls.get_cgi("owner_hash", True)
+        user_password_hash = cls.get_cgi("password_hash", True)
+        clip_hash = cls.get_cgi("clip_hash", True)
+        base_sync_hash = cls.get_cgi("base_sync_hash", True)
+        new_sync_hash = cls.get_cgi("new_sync_hash", True)
+        sync_patch = cls.get_cgi("sync_patch", True)
+
+        if cls.error:
+            return cls.result
+
+        # ----------------------------------------------------------------
+        # check user
+
+        if cls.check_user(user_hash, user_password_hash) is False:
+            return cls.result
+
+        # ----------------------------------------------------------------
+        # auth
+
+        auth = cls.DB.select(
+            table="clips",
+            column=[
+                "owner_hash",
+                "clip_mode"
+            ],
+            where={
+                "hash": clip_hash
+            }
+        )
+
+        auth = auth[0]
+        if auth["owner_hash"] != user_hash:
+            if auth["clip_mode"] == 'private':
+                cls.result["error"] = cls._error("permission_denied")
+                return cls.result
+
+        # ----------------------------------------------------------------
+        # select patches
+
+        where = {
+            "hash": [
+                "access", "<>"
+            ],
+            "clip_hash": clip_hash
+        }
+
+        _base_created_at = cls.DB.select(
+            table="syncs",
+            column="created_at",
+            where={
+                "hash": [
+                    "access", "<>"
+                ],
+                "base_hash": base_sync_hash
+            },
+            order={
+                "created_at": "DESC"
+            }
+        )
+
+        cls.result["patches"] = []
+
+        if len(_base_created_at) > 0:
+            where["created_at"] = [
+                _base_created_at[0]["created_at"], ">="
+            ]
+
+            cls.result["patches"] = cls.DB.select(
+                table="syncs",
+                column=[
+                    "hash",
+                    "base_hash",
+                    "patch"
+                ],
+                where=where,
+                order={
+                    "created_at": "ASC"
+                }
+            )
+
+        # ----------------------------------------------------------------
+        # new sync
+
+        new_sync = {
+            "clip_hash": clip_hash,
+            "owner_hash": user_hash,
+            "base_hash": base_sync_hash,
+            "created_at": cls.get_date()
+        }
+
+        if new_sync_hash is not None:
+            new_sync["hash"] = new_sync_hash
+            new_sync["patch"] = sync_patch
+
+        cls.result["sync"] = cls.DB.insert(
+            table="syncs",
+            value=new_sync
+        )
+
+        # ----------------------------------------------------------------
+        # select member
+
+        where = {
+            "clip_hash": clip_hash
+        }
+
+        cls.result["member"] = cls.DB.select(
+            table="syncs",
+            distinct=True,
+            column=[
+                "owners.hash AS user_hash",
+                "owners.username AS user_name",
+                "owners.email_address AS user_gravatar"
+            ],
+            where={
+                "clip_hash": clip_hash,
+                "syncs.created_at": [
+                    cls.get_date(datetime.datetime.now() - datetime.timedelta(minutes=1)),
+                    ">"
+                ]
+            },
+            join_table="owners",
+            join_key="hash"
+        )
+
+        if isinstance(cls.result["member"], list):
+            for index in range(len(cls.result["member"])):
+                cls.result["member"][index]["user_gravatar"] = cls.get_md5(cls.result["member"][index]["user_gravatar"])
+
+        # ----------------------------------------------------------------
         # return
 
         cls.result["result"] = True
